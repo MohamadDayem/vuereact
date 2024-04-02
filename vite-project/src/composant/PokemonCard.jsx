607ee3aa18baf52d97ead4280191c58388b6cd1a
mohamaddayem <u@example.com>
2024-04-02 14:11:02 +0300

    quetes2

---
+++ vite-project/src/composant/PokemonCard.jsx
@@ -0,0 +1,12 @@
+function PokeCard() {
+    return (
+      <div>
+       <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bullbasur" />
+       </figure>
+       <figcaption>Bulbasaur</figcaption>
+      </div>
+    );
+  }
+  
+  export default PokeCard;
