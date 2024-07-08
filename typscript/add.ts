a4908b71b92bd060ca46673856c5562527c8bdb3
mohamaddayem <u@example.com>
2024-07-08 14:16:32 +0300

    m

--- typscript/add.ts
+++ typscript/add.ts
@@ -1,15 +1,15 @@
-function hello(name) {
+function hello(name: string): void {
     console.log("Hello " + name);
 }
 
-const firstName = "bob";
+const firstName: string = "bob";
 
 hello(firstName);
 hello(firstName + " marley");
 
-function concat(a, b) {
+function concat(a: string, b: string): string {
     return a + b;
 }
 
-const wcs = concat("Wild", concat("Code", "School"));
+const wcs: string = concat("Wild", concat("Code", "School"));
 console.log(wcs);
